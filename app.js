6bc7d795780b38b959ba435efc588ef1e155ba41
ruidong <crd1309399974@163.com>
2018-12-05 16:10:54 +0800

    first commit

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ fs.readdirSync(path.join(__dirname, 'module')).reverse().forEach(file => {
     })
 })
 
-const port = process.env.PORT || 80
+const port = process.env.PORT || 8080
 
 app.server = app.listen(port, () => {
     console.log(`server running @ http://localhost:${port}`)
